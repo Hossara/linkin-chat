1cc743af58f238d9289ec695c16f165ee8cbc75f
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-21 20:36:02 +0330

    feat: Add database initialization to app

--- app/app.go
+++ app/app.go
@@ -2,7 +2,7 @@ package app
 
 import (
 	"github.com/Hossara/linkin-chat/config"
-	"gorm.io/gorm"
+	"github.com/Hossara/linkin-chat/pkg/postgres"
 )
 
 type app struct {
@@ -11,16 +11,37 @@ type app struct {
 }
 
 func (a *app) DB() *gorm.DB {
-	//TODO implement me
-	panic("implement me")
+	return a.db
 }
 
 func (a *app) Config() config.ServerConfig {
 	return a.cfg
 }
 
+func (a *app) setDB() error {
+	db, err := postgres.NewPsqlGormConnection(postgres.DBConnOptions{
+		Host:   a.cfg.DB.Host,
+		Port:   a.cfg.DB.Port,
+		User:   a.cfg.DB.User,
+		Pass:   a.cfg.DB.Pass,
+		Name:   a.cfg.DB.Name,
+		Schema: a.cfg.DB.Schema,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	a.db = db
+	return nil
+}
+
 func NewApp(cfg config.ServerConfig) (App, error) {
 	a := &app{cfg: cfg}
+
+	if err := a.setDB(); err != nil {
+		return nil, err
+	}
 
 	return a, nil
 }
