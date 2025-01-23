90e8f0f27ee844b672e0723289a17e8ff7184d74
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-23 20:02:40 +0330

    fix(user): Remove unused type

--- pkg/adapters/database/models/user.go
+++ pkg/adapters/database/models/user.go
@@ -4,11 +4,9 @@ import (
 	"gorm.io/gorm"
 )
 
-type UserID uint
-
 type User struct {
 	gorm.Model
-	ID        UserID `gorm:"primaryKey;autoIncrement"`
+	ID        uint   `gorm:"primaryKey;autoIncrement"`
 	Username  string `gorm:"not null;unique"`
 	Password  string `gorm:"not null"`
 	FirstName string `gorm:"size:100;not null"`
