f45c8e81fc83fa7e3ecb8aa8b90f22dddcb87e8e
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-25 01:09:58 +0330

    feat(chat): Implement CreateNewChat chat api service method

--- server/http/services/chat.go
+++ server/http/services/chat.go
@@ -3,11 +3,21 @@ package services
 import (
 	"context"
 	"github.com/Hossara/linkin-chat/app"
-	chatPort "github.com/Hossara/linkin-chat/internal/chat/port"
-	userDomain "github.com/Hossara/linkin-chat/internal/user/domain"
+	"github.com/Hossara/linkin-chat/internal/chat/domain"
+	"github.com/Hossara/linkin-chat/pkg/utils"
 	"github.com/Hossara/linkin-chat/server/http/helpers"
 	"github.com/Hossara/linkin-chat/server/http/mapper"
 	"github.com/Hossara/linkin-chat/server/http/types"
+
+	chatService "github.com/Hossara/linkin-chat/internal/chat"
+	chatPort "github.com/Hossara/linkin-chat/internal/chat/port"
+	userDomain "github.com/Hossara/linkin-chat/internal/user/domain"
+)
+
+var (
+	ErrMaximumChatReached = chatService.ErrMaximumChatReached
+	ErrInvalidChatInfo    = chatService.ErrInvalidChatInfo
+	ErrInvalidUserID      = chatService.ErrInvalidUserID
 )
 
 type ChatService struct {
@@ -38,4 +48,22 @@ func (as *ChatService) GetAllChats(c context.Context, userID uint) (*types.AllCh
 	}
 
 	return mapper.ToResponseChatRoom(rooms), nil
+}
+
+func (as *ChatService) CreateNewChat(c context.Context, userID uint, title string) (*types.CreateNewChatResponse, error) {
+	title = utils.NormalizeString(title)
+
+	code, err := as.svc.CreateChatRoom(c, domain.ChatRoom{
+		Title:   title,
+		Users:   []userDomain.User{},
+		OwnerID: userDomain.UserID(userID),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.CreateNewChatResponse{
+		Code: string(code),
+	}, nil
 }
