a841497aacecb7895f060a58fe63ee58c376a352
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-25 00:47:01 +0330

    refactor(chat): Refactor bad practices in queries

--- pkg/adapters/database/chat_repo.go
+++ pkg/adapters/database/chat_repo.go
@@ -2,6 +2,9 @@ package database
 
 import (
 	"context"
+	"crypto/rand"
+	"errors"
+	"fmt"
 	chatDomain "github.com/Hossara/linkin-chat/internal/chat/domain"
 	"github.com/Hossara/linkin-chat/internal/chat/port"
 	"github.com/Hossara/linkin-chat/internal/user/domain"
@@ -28,22 +31,18 @@ func (r *chatRepo) Insert(ctx context.Context, room chatDomain.ChatRoom) error {
 }
 
 func (r *chatRepo) Count(ctx context.Context, userID domain.UserID) (int, error) {
-	panic("implement me")
-	/*var count int64
-	if err := r.db.Table("chat_users").Where("user_id = ?", chatID, userID).Count(&count).Error; err != nil {
-		return fmt.Errorf("failed to check if user exists in chat: %w", err)
-	}
-	if count > 0 {
-		return fmt.Errorf("user already exists in the chat")
+	var count int64
+
+	err := r.db.WithContext(ctx).
+		Model(&models.Chat{}).
+		Where("owner_id = ?", userID).
+		Count(&count).Error
+
+	if err != nil {
+		return 0, err
 	}
 
-	// Step 3: Add the user to the chat
-	user := User{ID: userID} // You only need the ID to associate it
-	if err := r.db.Model(&Chat{ID: chatID}).Association("Users").Append(&user); err != nil {
-		return fmt.Errorf("failed to add user to chat: %w", err)
-	}
-
-	return nil*/
+	return int(count), nil
 }
 
 func (r *chatRepo) Delete(ctx context.Context, code chatDomain.ChatRoomCode) error {
@@ -53,16 +52,18 @@ func (r *chatRepo) Delete(ctx context.Context, code chatDomain.ChatRoomCode) err
 
 func (r *chatRepo) FindAllByUserID(ctx context.Context, userID domain.UserID) ([]*chatDomain.ChatRoom, error) {
 	var chats []models.Chat
-	// Query to find all chats associated with the given userID
-	err := r.db.WithContext(ctx).Joins("JOIN chat_users ON chat_users.chat_id = chats.id").
-		Where("chat_users.user_id = ?", userID).Find(&chats).Error
+
+	err := r.db.WithContext(ctx).
+		Where("owner_id = ?", userID).
+		Preload("Users").
+		Preload("Owner").
+		Find(&chats).Error
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to fetch user chats: %w", err)
 	}
 
-	// Transform database models to domain models
-	chatRooms := utils.Map(chats, func(t models.Chat) *chatDomain.ChatRoom {
-		return mapper.ToDomainChat(&t)
+	chatRooms := utils.Map(chats, func(chat models.Chat) *chatDomain.ChatRoom {
+		return mapper.ToDomainChat(&chat)
 	})
 
 	return chatRooms, nil
