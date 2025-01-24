f393712515bb0460c42415e03440fd45ced0f589
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-25 00:47:48 +0330

    feat(chat): Implement insert and generate chat code repository methods

--- pkg/adapters/database/chat_repo.go
+++ pkg/adapters/database/chat_repo.go
@@ -12,9 +12,17 @@ import (
 	"github.com/Hossara/linkin-chat/pkg/adapters/database/mapper"
 	"github.com/Hossara/linkin-chat/pkg/adapters/database/models"
 	"github.com/Hossara/linkin-chat/pkg/utils"
-
 	"github.com/go-gormigrate/gormigrate/v2"
 	"gorm.io/gorm"
+)
+
+var (
+	ErrMaximumChatReached = errors.New("maximum chat reached")
+)
+
+const (
+	codeLength = 8
+	charset    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 )
 
 type chatRepo struct {
@@ -26,8 +34,59 @@ func NewChatRepo(db *gorm.DB) port.ChatRepo {
 }
 
 func (r *chatRepo) Insert(ctx context.Context, room chatDomain.ChatRoom) error {
-	//TODO implement me
-	panic("implement me")
+	count, err := r.Count(ctx, room.OwnerID)
+	if err != nil {
+		return fmt.Errorf("failed to count chats for user %d: %w", room.ID, err)
+	}
+
+	if count >= 5 {
+		return ErrMaximumChatReached
+	}
+
+	chat := mapper.ToModelChat(&room)
+
+	chat.Owner = models.User{ID: uint(room.OwnerID)}
+
+	// Insert the chat into the database
+	if err := r.db.WithContext(ctx).Create(&chat).Error; err != nil {
+		return fmt.Errorf("failed to insert chat room: %w", err)
+	}
+
+	return nil
+}
+
+func generateRandomCode() (string, error) {
+	b := make([]byte, codeLength)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate random code: %w", err)
+	}
+
+	for i := range b {
+		b[i] = charset[b[i]%byte(len(charset))]
+	}
+
+	return string(b), nil
+}
+
+func (r *chatRepo) GenerateNewCode(ctx context.Context) (chatDomain.ChatRoomCode, error) {
+	for {
+		code, err := generateRandomCode()
+		if err != nil {
+			return "", err
+		}
+
+		// Check if the code is unique in the database
+		var count int64
+		err = r.db.WithContext(ctx).Model(&models.Chat{}).Where("code = ?", code).Count(&count).Error
+		if err != nil {
+			return "", fmt.Errorf("failed to verify code uniqueness: %w", err)
+		}
+
+		if count == 0 {
+			return chatDomain.ChatRoomCode(code), nil
+		}
+	}
 }
 
 func (r *chatRepo) Count(ctx context.Context, userID domain.UserID) (int, error) {
