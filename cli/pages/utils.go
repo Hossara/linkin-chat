96c2f3ac4dc25a3647c19943fb10c8e02f030e56
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-25 02:34:02 +0330

    feat(pages): Implement navigate, page render and capture focus utils

---
+++ cli/pages/utils.go
@@ -0,0 +1,44 @@
+package pages
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+func RenderPage(app *tview.Application, view tview.Primitive) {
+	if err := app.SetRoot(view, true).Run(); err != nil {
+		panic(err)
+	}
+}
+
+func NavigateTo(pages *tview.Pages, pageName string) {
+	if pages.HasPage(pageName) {
+		pages.SwitchToPage(pageName)
+	}
+}
+
+func RemoveAndNavigate(pages *tview.Pages, app *tview.Application, name string) {
+	if pages.HasPage(name) {
+		pages.RemovePage(name)
+		pages.AddAndSwitchToPage(name, GetPage(name)(app, pages), true)
+	}
+}
+
+type CaptureType func(event *tcell.EventKey) *tcell.EventKey
+
+func CaptureFocus(app *tview.Application, currentFocus int, focusList []tview.Primitive) CaptureType {
+	return func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyTab: // Move to the next focusable item
+			currentFocus = (currentFocus + 1) % len(focusList)
+			app.SetFocus(focusList[currentFocus])
+			return nil
+		case tcell.KeyBacktab: // Move to the previous focusable item
+			currentFocus = (currentFocus - 1 + len(focusList)) % len(focusList)
+			app.SetFocus(focusList[currentFocus])
+			return nil
+		default:
+		}
+		return event
+	}
+}
