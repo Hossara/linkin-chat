6818a454b4ab6e51f11c04e4132f1411bcfc4d9e
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-24 18:40:18 +0330

    refactor(jwt pkg): Remove unused method, change signing algorithm, Implement extract user claim method

--- server/http/server.go
+++ server/http/server.go
@@ -1,9 +1,14 @@
 package http
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+	"log"
 
 	"github.com/Hossara/linkin-chat/app"
 	"github.com/Hossara/linkin-chat/config"
@@ -19,12 +24,28 @@ func Bootstrap(ac app.App, cfg config.Server) error {
 		JSONDecoder: json.Unmarshal,
 	})
 
-	api := fiberApp.Group("/auth")
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		log.Fatalf("Error generating private key: %v", err)
+	}
 
-	accountSvcGetter := services.AccountServiceGetter(ac, cfg)
+	publicKey := &privateKey.PublicKey
+	viper.Set("public_key", publicKey)
+	viper.Set("private_key", privateKey)
 
-	api.Post("/login", middlewares.RateLimiter(), handlers.Login(accountSvcGetter))
-	api.Post("/register", middlewares.RateLimiter(), handlers.Register(accountSvcGetter))
+	authMiddleware := middlewares.Authorization(publicKey)
+	reteLimiter := middlewares.RateLimiter()
+
+	authGroup := fiberApp.Group("/auth", reteLimiter)
+	chatGroup := fiberApp.Group("/chat", reteLimiter)
+
+	accountSvcGetter := services.AccountServiceGetter(ac, cfg, privateKey)
+	chatSvcGetter := services.ChatServiceGetter(ac, cfg)
+
+	authGroup.Post("/login", handlers.Login(accountSvcGetter))
+	authGroup.Post("/register", handlers.Register(accountSvcGetter))
+
+	chatGroup.Get("/", authMiddleware, handlers.GetAllChats(chatSvcGetter))
 
 	//api.Post("/nats", NatsAuth(accountSvcGetter))
 
