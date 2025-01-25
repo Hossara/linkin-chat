b3d849b347d3d1662befda96c8500b6d573b3118
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-25 03:42:05 +0330

    feat(chat): Basically implement chat page

--- cli/pages/chat.go
+++ cli/pages/chat.go
@@ -1,9 +1,19 @@
 package pages
 
-import "github.com/rivo/tview"
+import (
+	"github.com/rivo/tview"
+	"github.com/spf13/viper"
+)
 
 func ChatPage(app *tview.Application, pages *tview.Pages) tview.Primitive {
+
 	root := tview.NewFlex().SetDirection(tview.FlexRow)
 
-	return root
+	chatCode := viper.GetString("chat.code")
+
+	if chatCode == "" {
+		RemoveAndNavigate(pages, app, "welcome")
+	}
+
+	return root.AddItem(tview.NewTextView().SetText(chatCode), 0, 1, false)
 }
